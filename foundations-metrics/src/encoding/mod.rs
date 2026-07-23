59953b52fa5d2fb31ece31ff0c7ea5dbf5b45846
Ethan Olchik <eolchik@cloudflare.com>
2026-07-23 17:49:08 +0100

    refactor(metrics): remove summary protobuf encoding

--- foundations-metrics/src/encoding/mod.rs
+++ foundations-metrics/src/encoding/mod.rs
@@ -1,5 +1,6 @@
 mod text;
 
+use foundations_metrics_registry::proto::MetricType;
 use prost::Message;
 
 use crate::MetricFamily;
@@ -10,6 +11,30 @@ pub use text::encode_to_text;
 pub fn encode_to_protobuf(families: &[MetricFamily]) -> Vec<u8> {
     families
         .iter()
+        .filter(|family| family.r#type != Some(MetricType::Summary as i32))
         .flat_map(Message::encode_length_delimited_to_vec)
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use foundations_metrics_registry::proto::Metric;
+
+    use super::*;
+
+    #[test]
+    fn omits_summary_families() {
+        let families = [MetricFamily {
+            name: Some("request_size".to_owned()),
+            help: Some("Request size.".to_owned()),
+            r#type: Some(MetricType::Summary as i32),
+            metric: vec![Metric {
+                summary: Some(Default::default()),
+                ..Default::default()
+            }],
+            unit: None,
+        }];
+
+        assert!(encode_to_protobuf(&families).is_empty());
+    }
 }
