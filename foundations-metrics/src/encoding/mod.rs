806ad2cdefed0f0cbcdc4ed846e8029b232b23b9
Ethan Olchik <eolchik@cloudflare.com>
2026-07-23 18:28:41 +0100

    test(metrics): cover legacy info metric encoding

--- foundations-metrics/src/encoding/mod.rs
+++ foundations-metrics/src/encoding/mod.rs
@@ -18,7 +18,7 @@ pub fn encode_to_protobuf(families: &[MetricFamily]) -> Vec<u8> {
 
 #[cfg(test)]
 mod tests {
-    use foundations_metrics_registry::proto::Metric;
+    use foundations_metrics_registry::proto::{Gauge, LabelPair, Metric};
 
     use super::*;
 
@@ -36,5 +36,28 @@ mod tests {
         }];
 
         assert!(encode_to_protobuf(&families).is_empty());
+    }
+
+    #[test]
+    fn preserves_legacy_info_gauge_representation() {
+        let families = [MetricFamily {
+            name: Some("build_info".to_owned()),
+            help: Some("Build information.".to_owned()),
+            r#type: Some(MetricType::Gauge as i32),
+            metric: vec![Metric {
+                label: vec![LabelPair {
+                    name: Some("version".to_owned()),
+                    value: Some("1.2.3".to_owned()),
+                }],
+                gauge: Some(Gauge { value: Some(1.0) }),
+                ..Default::default()
+            }],
+            unit: None,
+        }];
+
+        let encoded = encode_to_protobuf(&families);
+        let decoded = MetricFamily::decode_length_delimited(encoded.as_slice()).unwrap();
+
+        assert_eq!(decoded, families[0]);
     }
 }
